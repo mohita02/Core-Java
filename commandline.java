6bb9ae7243dc4ca99a9b7c6dd7f51afeaad32c78
Mohita Chaudhary <mohitachaudhary02@gmail.com>
2018-07-18 00:15:15 +0530

    Add files via upload

---
+++ commandline.java
@@ -0,0 +1,11 @@
+public class commandline
+{
+    public static void main(String a[])
+    {
+        int n=a.length;
+        for(int i=0;i<n;i++)
+        {
+            System.out.println(a[i]);
+        }
+    }
+}
