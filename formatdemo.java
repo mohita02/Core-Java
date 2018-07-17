b4198d0eb6574a527814f41f9bb7e17254bb0876
Mohita Chaudhary <mohitachaudhary02@gmail.com>
2018-07-18 00:13:23 +0530

    Add files via upload

---
+++ formatdemo.java
@@ -0,0 +1,10 @@
+class formatdemo{
+    public static void main(String a[])
+    {
+        String str="Hello";
+        int i=52;
+        float f = 55.55f;
+        boolean b=true;
+        System.out.printf("String = %s %n integer = %d %n float = %f %n  boolean = %b %n",str,i,f,b);
+    }
+}
